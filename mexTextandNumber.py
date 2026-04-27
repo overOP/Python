7a5dbf0bdd52b9e79efda8f106a299a0e4a8b47e
Pradeep Chaudhary <cgpradeepop@gmail.com>
2026-04-27 21:59:00 +0545

    parameter and mex the test and Number

---
+++ mexTextandNumber.py
@@ -0,0 +1,10 @@
+print(1)
+print("389")
+print(39)
+
+
+print(22 + 11)
+
+print("22" + "11")
+
+print("I am", 24,"years old")
---
+++ parameter.py
@@ -0,0 +1,4 @@
+# this is a parameter ( end=" " )
+print("hello", end=" ")
+print("my name is", end=" ")
+print("pradeep")
