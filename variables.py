aa7692e12c12d4cd6901be1e472b9fa5902c03d4
Pradeep Chaudhary <cgpradeepop@gmail.com>
2026-04-28 22:34:03 +0545

    variables

--- variables.py
+++ variables.py
@@ -1,8 +1,65 @@
-x = 5
-y = "pradeep"
-print(x);
-print(y);
-
-
-x = 4
+x = 20
+x = "pradeep"
+print(x)
 print(type(x))
+
+a = str(3)
+b = int(3)
+c = float(3)
+
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+
+
+
+d = "pradeep"
+e = 'pradeep'
+f = """pradeep"""
+
+print(d)
+print(type(d))
+print(e)
+print(type(e))
+print(f)
+print(type(f))
+
+r = 4
+R = "pradeep"
+print(r)
+print(R)
+print(type(r))
+print(type(R))
+
+
+x, y, z = 20, 30, 40
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+g = h = j = 10
+print(g)
+print(h)
+print(j)
+print(type(g))
+print(type(h))
+print(type(j))
+
+#unpacking
+fruits = ["apple", "banana", "cherry"]
+a1, a2, a3 = fruits
+print(a1)
+print(a2)
+print(a3)
+
+print(type(a1))
+print(type(a2))
+print(type(a3))
