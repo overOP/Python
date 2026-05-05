7c19237f2fee3247eed2f7e67ea8855e805f764e
Pradeep Chaudhary <over@Pradeeps-MacBook-Air.local>
2026-05-05 10:30:37 +0545

    k ho

--- hello.py
+++ hello.py
@@ -1,4 +1,14 @@
 if 5 > 2:
     print("5 is greater than 2")
+if 5 > 2:
+    print("5 is greater than 2")
     if 5 > 2:
         print("5 is greater than 2")
+if 5 > 2:
+    print("5 is greater than 2")
+if 5 > 2:
+    print("5 is greater than 2")
+if 5 > 2:
+    print("5 is greater than 2")
+if 5 > 2:
+    print("5 is greater than 2")
