1eff6beabce63841e4cb03b13d10f39433a899f6
Pradeep Chaudhary <cgpradeepop@gmail.com>
2026-05-06 13:41:38 +0545

    feat: add script to demonstrate Python numeric type conversion and inspection

---
+++ conversion.py
@@ -0,0 +1,23 @@
+x = 1
+y = 2.8
+z = 1j
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+a = float(x)
+b = int(y)
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
