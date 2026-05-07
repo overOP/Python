b461d7517471e3d8ea071b7d9469e8bc43389ae5
Pradeep Chaudhary <cgpradeepop@gmail.com>
2026-05-07 11:56:11 +0545

    feat: add script demonstrating type casting for integers, floats, and strings

---
+++ casting.py
@@ -0,0 +1,41 @@
+# Integers:
+x = int(1)
+y = int(2.8)
+z = int("3")
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Float:
+x = float(1)
+y = float(2.8)
+z = float("3")
+a = float("4.2")
+
+print(x)
+print(y)
+print(z)
+print(a)
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+
+# String:
+x = str("s1")
+y = str(2)
+z = str(3.0)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
