68aeb1d92c1c9664932238af742e3bb4404f669d
Pradeep Chaudhary <cgpradeepop@gmail.com>
2026-05-08 13:14:42 +0545

    feat: add examples for slicing from start, to end, and negative slicing in Slicing.py

--- Slicing.py
+++ Slicing.py
@@ -2,3 +2,22 @@ b = "Hello, World!"
 # H e l l o ,   W o r l d !
 # 0 1 2 3 4 5 6 7 8 9 101112
 print(b[2:5])
+
+# Slicing from start
+a = "Hello, World!"
+# H e l l o ,   W o r l d !
+# 0 1 2 3 4 5 6 7 8 9 101112
+print(a[:5])
+
+# Slicing to end
+a = "Hello, World!"
+# H e l l o ,   W o r l d !
+# 0 1 2 3 4 5 6 7 8 9 101112
+print(a[7:])
+
+# Negative Slicing
+a = "Hello, World!"
+# H e l l o ,   W o r l d !
+# 0 1 2 3 4 5 6 7 8 9 101112
+# -1 -2 -3 -4 -5 -6 -7 -8 -9 -10111213
+print(a[-5:])
