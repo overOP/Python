c98d29047c2eb1d22e0e235d64827a9d96bfa220
Pradeep Chaudhary <cgpradeepop@gmail.com>
2026-05-10 18:39:02 +0545

    feat: add demonstration script for Python f-string formatting and operations

---
+++ F-Strings.py
@@ -0,0 +1,20 @@
+age = 24
+name = "Pradeep"
+txt = f"My name is {name} and I am {age} years old."
+print(txt)
+
+price = 19
+txt = f"The price is {price} dollars."
+print(txt)
+
+
+# modifiers use of : and .2f it will show in 2 decimal place
+price = 19
+txt = f"The price is {price:.2f} dollars."
+print(txt)
+
+# math operations
+age = 24
+name = "Pradeep"
+txt = f"My name is {name} and I am {age+2} years old."
+print(txt)
