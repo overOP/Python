e3e232d3554fae141ea4321800cc440d3c069372
Pradeep Chaudhary <cgpradeepop@gmail.com>
2026-05-10 18:59:51 +0545

    feat: add examples demonstrating usage of various Python escape characters

---
+++ Escape_Character.py
@@ -0,0 +1,48 @@
+# Escape Character "
+txt = "python is \"awesome\""
+print(txt)
+
+# Escape Character \n
+txt = "python is \nawesome"
+print(txt)
+
+
+# Escape Character \t
+txt = "python is \tawesome"
+print(txt)
+
+
+# Escape Character \b
+txt = "python is \bawesome"
+print(txt)
+
+
+# Escape Character \r
+txt = "python is \rawe some"
+print(txt)
+
+#Escape Character \f
+txt = "python is \fawesome"
+print(txt)
+
+#Escape Character \ooo (octal number)
+txt = "python is \111awesome"
+print(txt)
+
+
+#Escape Character \xhh (hexadecimal number)
+txt = "\x48\x65\x6c\x6c\x6f\x20\x57\x6f\x72\x6c\x64"
+print(txt)
+
+#Escape Character \x
+txt = "python is \x10awesome"
+print(txt)
+
+#Escape Character \'
+txt = "python is \'awesome'"
+print(txt)
+
+#Escape Character \\
+txt = "python is \\awesome"
+print(txt)
+
