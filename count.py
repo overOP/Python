dad8851e102f1836b997e9bfd2c182d19ed45cd3
Pradeep Chaudhary <cgpradeepop@gmail.com>
2026-05-13 22:23:33 +0545

    feat: add script to demonstrate string count functionality with index constraints

---
+++ count.py
@@ -0,0 +1,8 @@
+txt = "I love apples, apple are my favorite fruit"
+x = txt.count("apple")
+print(x)
+
+# count the apples in the range of 0 to 10
+txt = "I love apples, apple are my favorite fruit"
+x = txt.count("apple", 0, 10)
+print(x)
