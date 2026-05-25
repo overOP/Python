3e9fcf58183afc96f0aee6b233b74104f1aa53c9
Pradeep Chaudhary <cgpradeepop@gmail.com>
2026-05-25 22:20:11 +0545

    == and is

--- nothing.py
+++ nothing.py
@@ -1,3 +1,18 @@
 text = "hello"
 num = "12345"
 print(text[1] + num[3])
+
+a = 10
+b = 10
+if a == b:
+    print("a and b are equal")
+else:   
+    print("a and b are not equal")
+
+
+c = 10
+d = 10
+if c is d:
+    print("c and d are the same object")
+else:
+    print("c and d are different objects")
